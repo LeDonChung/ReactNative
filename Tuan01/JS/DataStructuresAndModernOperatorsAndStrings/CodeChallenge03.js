aa8c17fe738df07df179c84e7b50cf511012d031
LeDonChung <ledonchung12a2@gmail.com>
2024-08-26 18:06:09 +0700

    Added: CodeChallenge03 in to DataStructuresAndModernOperatorsAndStrings

---
+++ Tuan01/JS/DataStructuresAndModernOperatorsAndStrings/CodeChallenge03.js
@@ -0,0 +1,56 @@
+// Let's continue with our football betting app! This time, we have a map called
+// 'gameEvents' (see below) with a log of the events that happened during the
+// game. The values are the events themselves, and the keys are the minutes in which
+// each event happened (a football game has 90 minutes plus some extra time).
+// Your tasks:
+// 1. Create an array 'events' of the different game events that happened (no
+// duplicates)
+// 2. After the game has finished, is was found that the yellow card from minute 64
+// was unfair. So remove this event from the game events log.
+// 3. Compute and log the following string to the console: "An event happened, on
+// average, every 9 minutes" (keep in mind that a game has 90 minutes)
+// 4. Loop over 'gameEvents' and log each element to the console, marking
+// whether it's in the first half or second half (after 45 min) of the game, like this:
+// [FIRST HALF] 17: ⚽ GOAL
+
+
+const gameEvents = new Map([
+    [17, '⚽ GOAL'],
+    [36, '� Substitution'],
+    [47, '⚽ GOAL'],
+    [61, '� Substitution'],
+    [64, '� Yellow card'],
+    [69, '� Red card'],
+    [70, '� Substitution'],
+    [72, '� Substitution'],
+    [76, '⚽ GOAL'],
+    [80, '⚽ GOAL'],
+    [92, '� Yellow card'],
+    ]);
+
+
+// Task 01: Create an array 'events' of the different game events that happened (no duplicates)
+// Copy vào Set thì chỉ lưu các value không trùng lặp
+const events = [...new Set(gameEvents.values())]
+console.log(events)
+
+// Task 2. After the game has finished, is was found that the yellow card from minute 64
+// was unfair. So remove this event from the game events log.
+gameEvents.delete(64)
+
+
+// Task 3. Compute and log the following string to the console: "An event happened, on
+// average, every 9 minutes" (keep in mind that a game has 90 minutes)
+const time = 90;
+const avgTime = time / gameEvents.size;
+
+console.log(`An event happened, on average, every ${avgTime} minutes`); 
+
+
+// Task 4. Loop over 'gameEvents' and log each element to the console, marking
+// // whether it's in the first half or second half (after 45 min) of the game, like this:
+// // [FIRST HALF] 17: ⚽ GOAL
+for(const [m, e] of gameEvents) {
+    const half = m <= 45 ? 'FIRST HALF' : 'SECOND HALF';
+    console.log(`[${half}] ${m}: ${e}`);
+}
